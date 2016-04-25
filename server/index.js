0ab3efc6edc1ffe96b8ee8e7eecd57de60eca412
Brian Cameron <brian.cameron@gmail.com>
2016-04-25 13:27:57 +0200

    Playing with CSS. Tidy up server code.

--- server/index.js
+++ server/index.js
@@ -6,26 +6,23 @@ import morgan from 'morgan'
 import nunjucks from 'nunjucks'
 
 import {reload} from './gulp'
+
 import Db from './db'
-
-// Database
 const db = new Db('db.json')
-// db.drop('todos')
 
-// Server App
+// Configure Express
 const app = express()
 app.use(morgan('dev'))
 app.use(require('connect-livereload')());
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
-
 nunjucks.configure('server/views', {
   autoescape: true,
   express: app,
   noCache: true
 })
 
-// Web Application (Server Side)
+// Web Application (Server Side Render)
 app.get('/todos', function(req, res) {
   res.render('todos.html', {todos: db.all('todos')})
 })
@@ -34,12 +31,13 @@ app.post('/todos', function(req, res) {
   res.redirect('/todos')
 })
 
-// Json API
+// Json API for Client Side Apps
 app.get('/todos.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json')
   res.send({todos: db.all('todos')})
 })
 
+// Bootup Server
 app.listen(3000, () => {
   console.log("Listening on port 3000")
   setTimeout(reload, 2000)
